4b94808ca0b83e7fd5e6396284df336b2ec0c8ff
Oleh Havryliuk <om.havryliuk@gmail.com>
2024-04-02 23:50:34 +0300

    deployed site on hosting

--- database.php
+++ database.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "businesscard_db";
-// $servername = "sql108.infinityfree.com";
-// $username = "if0_36292191";
-// $password = "xEVAM5Vt5yNrCWP";
-// $dbname = "if0_36292191_businesscard";
+// $servername = "127.0.0.1";
+// $username = "root";
+// $password = "";
+// $dbname = "businesscard_db";
+$servername = "sql108.infinityfree.com";
+$username = "if0_36292191";
+$password = "xEVAM5Vt5yNrCWP";
+$dbname = "if0_36292191_businesscard";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
