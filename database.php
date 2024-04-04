a1a5dfc3e0f549f02b5f024bf2f5756e5b2ba7d8
Oleh Havryliuk <om.havryliuk@gmail.com>
2024-04-04 20:41:34 +0300

    deleted secret data

--- database.php
+++ database.php
@@ -1,12 +1,11 @@
 <?php
-// $servername = "127.0.0.1";
-// $username = "root";
-// $password = "";
-// $dbname = "businesscard_db";
-$servername = "sql108.infinityfree.com";
-$username = "if0_36292191";
-$password = "xEVAM5Vt5yNrCWP";
-$dbname = "if0_36292191_businesscard";
+include "config.php";
+$servername = $config['servername'];
+$username = $config['username'];
+$password = $config['password'];
+$dbname = $config['dbname'];
+
+session_start();
+session_regenerate_id(true);
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-?>
